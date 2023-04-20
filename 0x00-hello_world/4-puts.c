f2f32adb5cea2997562c7982b1805c28a2cc67ab
Edith Abofra <edith.abofra@gmail.com>
2023-04-20 15:37:56 -0700

    Using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*
+ * This program prints a line with pus function
+ * Return: Always 0 (SuccessL
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+		return(0)
