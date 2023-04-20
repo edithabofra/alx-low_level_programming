155785b45469897d75a934cf7ed2d3a3806fd877
Edith Abofra <edith.abofra@gmail.com>
2023-04-20 16:08:39 -0700

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Return: Always0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece iof art,\n");
+	return (0);
+}
