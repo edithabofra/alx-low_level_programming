e5ad8e1086cd90eaac6d5554d1382fae08bde01f
Edith Abofra <edith.abofra@gmail.com>
2023-04-20 15:54:20 -0700

    Using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * This program prints a line with pus function
  * Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-		return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
