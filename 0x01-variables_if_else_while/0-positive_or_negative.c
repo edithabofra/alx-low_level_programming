c8b671e5a9d2e0490783a07c55c9dcb657376ae5
Edith Abofra <edith.abofra@gmail.com>
2023-04-21 12:53:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main - Code to determine if number is negative, zero or pisitve
+
+/**
+ * main - etermine if number is negative, zero or pisitve
+ * 
  * Return: Always 0 (Success)
 */
 int main(void)
