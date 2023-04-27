06fbb7a20dcd005a0cb4e876751fc03545d053f1
Edith Abofra <edith.abofra@gmail.com>
2023-04-26 18:21:54 -0700

    Updates

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,24 +4,24 @@
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 for the positive num,
- * -1 for the negative num or zero for anything else
+ *-1 for the negative num or zero for anything else
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
-		return (1);
+	_putchar(43);
+	return (1);
 	}
 	else if (n > 0)
 	{
-		_putchar(45);
-		return (-1);
+	_putchar(45);
+	return (-1);
 	}
 	else
 	{
-		_putchar(48);
-		return (0);
+	_putchar(48);
+	return (0);
 	}
 }
