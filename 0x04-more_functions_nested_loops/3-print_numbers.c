e3b56f948d2b940f5eea1554ab77e87425de3a91
Edith Abofra <edith.abofra@gmail.com>
2023-04-29 18:27:49 -0700

    prints numbers from 0 to 0

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers -prints numbers from 0-9
- * Return; void
+ * print_numbers - prints numbers  0-9
+ * return: void
  */
 
 void print_numbers(void)
 {
-	int c;
+	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(c + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
