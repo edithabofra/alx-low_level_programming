14b7be02240c41c4f004b172246d3787c9ec93da
Edith Abofra <edith.abofra@gmail.com>
2023-05-03 13:24:25 -0700

    Integers and pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *reset_to_98 - Takes a pointer to an interger and updated value
+ *
+ * @n: The integer
+ * Return : nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
